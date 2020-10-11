0ccda74ed6ebffc4a78b5551a952794f3bea446a
Mateusz Grabowski <m.grabowski@rolladengroup.de>
2020-10-11 20:28:21 +0200

    iframe and image testing

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,15 @@
 const lightboxOne = new Lightbox({
     selector: '.iframe',
-  });
+    type: 'iframe',
+});
+
+const lightboxTwo = new Lightbox({
+  selector: '.iframe2',
+  type: 'iframe',
+});
+
+
+const lightboxThree = new Lightbox({
+  selector: '.image',
+  type: 'image',
+});
--- src/js/lightbox.js
+++ src/js/lightbox.js
@@ -2,15 +2,56 @@ class Lightbox {
   constructor(object) {
     this.selector = object.selector;
     this.triger = document.querySelector(object.selector);
-    this.click();
+    this.type = object.type;
+    this.id = Date.now() + Math.floor(Math.random() * 100);
+    this.eventListener();
   }
 
-  click() {
-    this.triger.addEventListener('click', (e) => {
-      e.preventDefault();
-      const iframe = document.createElement('iframe');
-      iframe.src = e.target.href;
-      document.body.appendChild(iframe);
+  eventListener() {
+    document.addEventListener('click', (e) => {
+
+      if (e.target.closest(this.selector)) {
+        e.preventDefault();
+        this.crateLightbox();
+      }
+
+      if (e.target.matches('.lightbox') || e.target.matches('.lightbox__close-btn')) {
+        this.removeLightbox();
+      }
     });
+
+    document.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape') {
+        this.removeLightbox();
+      }
+    });
+
+  }
+
+  crateLightbox() {
+    const box = document.createElement('div');
+    box.classList.add('lightbox');
+    box.dataset.id = this.id;
+    let content;
+
+    if (this.type === 'iframe') {
+      content =  `<iframe class="lightbox__iframe" src="${this.triger.href}"><iframe>`
+    } else if (this.type === 'image') {
+      content= `<img class="lightbox__img" src="${this.triger.href}">`
+    }
+
+    box.innerHTML = `
+      <div class="lightbox__content lightbox__content--${this.type}">
+        <button class="lightbox__close-btn"></button>
+        ${content}
+      </div>
+    `
+
+    document.body.appendChild(box);
+  }
+
+  removeLightbox() {
+    const lightbox = document.querySelector(`.lightbox[data-id="${this.id}"]`);
+    if (lightbox) lightbox.remove();
   }
 }
